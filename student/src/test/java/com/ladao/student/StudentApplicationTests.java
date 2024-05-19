ff9abffd5285fe0d9b3a30d763316a7575e4c1d8
UA-uzrheinreyes <umreyes.student@ua.edu.ph>
2024-05-19 22:42:39 +0800

    first commit

---
+++ student/src/test/java/com/ladao/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ladao.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
